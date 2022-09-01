86b6bce6ab36ba73f81b76a4ff03dbc5b1781a6d
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2022-09-01 17:41:40 +0600

    Update ResourcePile.cs
    Encapsulated ProductionSpeed with a getter and a setter to prevent negative production speeds.

--- Assets/Scripts/ResourcePile.cs
+++ Assets/Scripts/ResourcePile.cs
@@ -10,7 +10,25 @@ public class ResourcePile : Building
 {
     public ResourceItem Item;
 
-    public float ProductionSpeed = 0.5f;
+    private float m_ProductionSpeed = 0.5f; // add new private backing field. A private field that stores the data exposed by the public property. 
+
+    // With the backing field declared, you can now manually add the get and set functions, which retrieve or assign the backing field when someone accesses the public property.
+    // Since these are not going to be simple get and set methods, it’s not possible to use the “{ get; set; }” shorthand we used with the auto-implemented property earlier. 
+    public float ProductionSpeed // remove value from public property.
+    {
+        get { return m_ProductionSpeed; } // getter returns backing field
+        set {
+            if (value < 0.0f)
+            {
+
+                Debug.LogError("You can't set a negative production speed!");
+            }
+            else
+            {
+                m_ProductionSpeed = value; // original setter now in if/else statement
+            }
+        } // setter uses backing field
+    }
 
     private float m_CurrentProduction = 0.0f;
 
@@ -26,14 +44,14 @@ public class ResourcePile : Building
         
         if (m_CurrentProduction < 1.0f)
         {
-            m_CurrentProduction += ProductionSpeed * Time.deltaTime;
+            m_CurrentProduction += m_ProductionSpeed * Time.deltaTime; // // swap in m_ version
         }
     }
 
     public override string GetData()
     {
-        return $"Producing at the speed of {ProductionSpeed}/s";
-        
+        return $"Producing at the speed of {m_ProductionSpeed}/s"; // swap in m_ version
+
     }
     
     
